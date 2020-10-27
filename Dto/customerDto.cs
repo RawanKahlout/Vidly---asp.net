a8261c5843397d2682d1244ab6bf36033dd2f6d1
RawanKahlout <36929678+RawanKahlout@users.noreply.github.com>
2020-10-27 17:54:53 +0300

    Add files via upload

---
+++ Dto/customerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using myvidly.Models;
+namespace myvidly.Dto
+{
+    public class customerDto
+    {
+        
+        [Required(ErrorMessage = "Please Enter the name")]
+        public string customerName { get; set; }
+        public int id { get; set; }
+        public bool isSubscibedToNewsLetter { get; set; }
+        public membershipType membershipType { get; set; }//navagtion proprtey load entire object its usefull when we need to load objects togther
+        public byte membershipTypeId { get; set; }// mvc recognize this name and treat it as foreign key;
+        [minAge]
+        public Nullable<DateTime> BirthData { get; set; }
+    }
+}
